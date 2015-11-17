ca8371ad0fb1f0b477bf0cb6c2458c12db7628a8
Alexandre Martinez <alexandre.martinez76@gmail.com>
2015-11-17 14:01:56 +0100

    TEst registercommand

---
+++ Tests/Command/RegisterCommandTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Micro\BaseServiceBundle\Tests\Command;
+
+use Micro\BaseServiceBundle\Command\RegisterCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class ListCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecute()
+    {
+        $application = new Application();
+        $application->add(new RegisterCommand());
+
+        $command = $application->find('micro:service:register');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName()));
+
+        $this->assertRegExp('/.../', $commandTester->getDisplay());
+
+    }
+}
